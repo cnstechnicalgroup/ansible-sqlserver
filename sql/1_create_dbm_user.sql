b6c6a1a4d35dae1e2066ef1f569bf9e6a47e7d2a
vagrant <craig@cnstechgroup.com>
2017-06-02 14:45:31 +0000

    added check of mssql service status and added preliminary sql steps to create AG

---
+++ sql/1_create_dbm_user.sql
@@ -0,0 +1,2 @@
+CREATE LOGIN dbm_login WITH PASSWORD = 'TESTpasswordm837y2';
+CREATE USER dbm_user FOR LOGIN dbm_login;
---
+++ sql/2_PRIMARY_ONLY_create_master_key_on_primary.sql
@@ -0,0 +1,8 @@
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'TESTpasswordm837y2';
+CREATE CERTIFICATE dbm_certificate WITH SUBJECT = 'dbm';
+BACKUP CERTIFICATE dbm_certificate
+   TO FILE = '/var/opt/mssql/data/dbm_certificate.cer'
+   WITH PRIVATE KEY (
+           FILE = '/var/opt/mssql/data/dbm_certificate.pvk',
+           ENCRYPTION BY PASSWORD = 'TESTpasswordm837y2'
+       );
