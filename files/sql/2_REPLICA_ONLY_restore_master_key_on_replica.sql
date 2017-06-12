68384847a104fc47ecc23abb630307f455bf9524
vagrant <craig@cnstechgroup.com>
2017-06-12 15:54:35 +0000

    create ag on primary, secondary joins AG

--- files/sql/2_REPLICA_ONLY_restore_master_key_on_replica.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'TESTpasswordm837y2';
-CREATE CERTIFICATE dbm_certificate   
-    AUTHORIZATION dbm_user
-    FROM FILE = '/var/opt/mssql/data/dbm_certificate.cer'
-    WITH PRIVATE KEY (
-    FILE = '/var/opt/mssql/data/dbm_certificate.pvk',
-    DECRYPTION BY PASSWORD = 'TESTpasswordm837y2'
-            );
