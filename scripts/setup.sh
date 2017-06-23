749989fdd4390490fec9d3e9c301ec3c5ca286b7
Sam Morrison <sam@cnstechgroup.com>
2017-06-23 17:25:22 +0900

    create vagrant home dir in CI setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-yum install -y yum-utils
-yum-config-manager --add-repo=https://dl.fedoraproject.org/pub/epel/7/x86_64/
-yum install -y wget
-wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7 https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
-yum install -y python-devel libffi-devel openssl-devel gcc python-pip redhat-rpm-config
-pip install --upgrade pip
-pip install paramiko
-pip install ansible
+yum install -y yum-utils;
+yum-config-manager --add-repo=https://dl.fedoraproject.org/pub/epel/7/x86_64/;
+yum install -y wget;
+wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7 https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7;
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7;
+yum install -y python-devel libffi-devel openssl-devel gcc python-pip redhat-rpm-config;
+pip install --upgrade pip;
+pip install paramiko;
+pip install ansible;
+mkdir /home/vagrant;
